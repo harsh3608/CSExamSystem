8b893512bee802f0e84f7eed43f36ec6da26cfe1
harsh3608 <harsh3608@gmail.com>
2023-04-25 16:58:32 +0530

    Code optimization of modules

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminForgetPasswordComponent } from './admin-forget-password/admin-forget-password.component';
 import { QuestionsListComponent } from './questions/questions-list/questions-list.component';
@@ -10,32 +9,17 @@ import { TechnologiesListComponent } from './technologies/technologies-list/tech
 import { TechnologiesUpdateComponent } from './technologies/technologies-update/technologies-update.component';
 import { TechnologiesDetailComponent } from './technologies/technologies-detail/technologies-detail.component';
 import { QuestionsDetailComponent } from './questions/questions-detail/questions-detail.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { AdminMenuComponent } from './admin-menu/admin-menu.component';
-import {TableModule} from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { CKEditorModule } from 'ckeditor4-angular';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ExamsListComponent } from './exams/exams-list/exams-list.component';
 import { ExamsAddComponent } from './exams/exams-add/exams-add.component';
 import { AddUsersDialogComponent } from './exams/add-users-dialog/add-users-dialog.component';
-import { DropdownModule } from 'primeng/dropdown';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { PaginatorModule } from 'primeng/paginator';
 import { SharedModuleModule } from '../shared-module/shared-module.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { CandidatesDialogComponent } from './exams/candidates-dialog/candidates-dialog.component';
 import { ExamsRescheduleComponent } from './exams/exams-reschedule/exams-reschedule.component';
 import { CandidateResultsListComponent } from './candidate-results/candidate-results-list/candidate-results-list.component';
 import { CandidateResultDatailsComponent } from './candidate-results/candidate-result-datails/candidate-result-datails.component';
 import { RescheduleCandidateExamComponent } from './exams/reschedule-candidate-exam/reschedule-candidate-exam.component';
-import { TooltipModule } from 'primeng/tooltip';
-import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
   declarations: [
@@ -60,24 +44,8 @@ import { CalendarModule } from 'primeng/calendar';
     RescheduleCandidateExamComponent,
   ],
   imports: [
-    CommonModule,
-    BrowserModule,
     AdminRoutingModule,
-    ReactiveFormsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    TableModule,
-    InputTextModule,
-    ButtonModule,
-    CKEditorModule,
-    MatSlideToggleModule,
-    MatProgressSpinnerModule,
-    DropdownModule,
-    MatGridListModule,
-    PaginatorModule,
-    TooltipModule,
     SharedModuleModule,
-    CalendarModule
   ]
 })
 export class AdminModule { }
--- src/app/modules/shared-module/shared-module.module.ts
+++ src/app/modules/shared-module/shared-module.module.ts
@@ -5,6 +5,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "src/app/app.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { ToastrModule } from "ngx-toastr";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { Ng2SearchPipeModule } from "ng2-search-filter";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { TableModule } from "primeng/table";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
+import { CKEditorModule } from "ckeditor4-angular";
+import { DropdownModule } from "primeng/dropdown";
+import { PaginatorModule } from "primeng/paginator";
+import { TooltipModule } from "primeng/tooltip";
 
 
 @NgModule({
@@ -14,12 +32,53 @@ import { ToastrModule } from "ngx-toastr";
   imports:[
     CommonModule,
     BrowserModule,
-    //SharedModuleRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatGridListModule,
     HttpClientModule,
+    MatDialogModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    Ng2SearchPipeModule,
+    FormsModule,
+    MatProgressSpinnerModule,
     ToastrModule.forRoot(),
-
+    MatPaginatorModule,
+    TableModule,
+    InputTextModule,
+    ButtonModule,
+    CKEditorModule,
+    DropdownModule,
+    PaginatorModule,
+    TooltipModule,
   ],
   providers: [],
+  exports:[
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    Ng2SearchPipeModule,
+    FormsModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    TableModule,
+    InputTextModule,
+    ButtonModule,
+    CKEditorModule,
+    DropdownModule,
+    PaginatorModule,
+    TooltipModule,
+  ],
   bootstrap: [AppComponent]
 })
 
