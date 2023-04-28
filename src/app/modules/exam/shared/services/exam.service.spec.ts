467c2ac63d41d91ecd8f9ab9bf4a1987c84ff2aa
harsh3608 <harsh3608@gmail.com>
2023-04-28 18:45:25 +0530

    added test cases in user service and exam service

--- src/app/modules/exam/shared/services/exam.service.spec.ts
+++ src/app/modules/exam/shared/services/exam.service.spec.ts
@@ -1,16 +1,92 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ExamService } from './exam.service';
+import { ExamResponse, ExamTimeResponse, ExamUserStatus, ExamUserStatusResponse, FinishExamResponse } from '../models/exam.models';
 
-describe('ExamService', () => {
+fdescribe('ExamService', () => {
   let service: ExamService;
+  let examResponse: ExamResponse = {
+    statusCode: 0,
+    isSuccess: true,
+    response: [],
+    message: '',
+    exceptionMessage: ''
+  };
+  let examStatus: ExamUserStatus = {
+    id: 23,
+    selectedAnswerId: 76,
+    correctAnswer: 'A',
+    statusId: 4,
+    candidateExamUserId: 21,
+  };
+  let examUserStatusResponse: ExamUserStatusResponse = {
+    statusCode: 0,
+    isSuccess: true,
+    response: '',
+    message: '',
+    exceptionMessage: '',
+  };
+  let finishExamResponse: FinishExamResponse = {
+    statusCode: 0,
+    isSuccess: true,
+    response: {
+      createdBy: '',
+      createdOn: '',
+      modifiedBy: '',
+      lastModifiedOn: '',
+      id: 67,
+      userId: 'string',
+      candidateExamId: 243,
+      totalScore: 60,
+      obtainedScore: 45,
+      examDate: ''
+    },
+    message: '',
+    exceptionMessage: '',
+  };
+  let examTimeResponse: ExamTimeResponse = {
+    statusCode: 0,
+    isSuccess: true,
+    response: '',
+    message: '',
+    exceptionMessage: ''
+  }
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ExamService]
+    });
     service = TestBed.inject(ExamService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have loginCandidate function', () => {
+    service.getScheduledExams('user-4w5tvdf-vuwjk43jk.dv890').subscribe((res) => {
+      expect(res).toEqual(examResponse);
+    });
+  });
+
+  it('should have updateExamUserStatus function', () => {
+    service.updateExamUserStatus(examStatus).subscribe((res) => {
+      expect(res).toEqual(examUserStatusResponse);
+    });
+  });
+
+  it('should have updateExamUserStatus function', () => {
+    service.finishExam('user-4w5tvdf-vuwjk43jk.dv890').subscribe((res) => {
+      expect(res).toEqual(finishExamResponse);
+    });
+  });
+
+  it('should have updateExamUserStatus function', () => {
+    service.updateExamTime(45, true).subscribe((res) => {
+      expect(res).toEqual(examTimeResponse);
+    });
+  });
+
 });
--- src/app/modules/exam/shared/services/questions.service.spec.ts
+++ src/app/modules/exam/shared/services/questions.service.spec.ts
@@ -1,16 +1,103 @@
 import { TestBed } from '@angular/core/testing';
-
 import { QuestionsService } from './questions.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { SelectedAnswer, Status } from '../models/exam.models';
 
-describe('QuestionsService', () => {
+fdescribe('QuestionsService', () => {
   let service: QuestionsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [QuestionsService]
+    });
     service = TestBed.inject(QuestionsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('storeCheckedStatus', () => {
+    it('should add a new status object to the status array', () => {
+      const status: Status = {
+        index: 1,
+        id: 2,
+        checked: true,
+        optionId: 3,
+        selected: 'A',
+        candiateExamUserId: 4
+      };
+      //spyOn(localStorage, 'getItem').and.returnValue('[]');
+      spyOn(localStorage, 'setItem');
+
+      service.storeCheckedStatus(status);
+
+      const storedStatuses = JSON.parse(localStorage.getItem('status-array') || '{}');
+      expect(storedStatuses.length).toEqual(1);
+      expect(storedStatuses[0]).toEqual(status);
+      expect(localStorage.setItem).toHaveBeenCalledWith('status-array', JSON.stringify([status]));
+    });
+
+    it('should update an existing status object in the status array', () => {
+      const existingStatus: Status = {
+        index: 1,
+        id: 2,
+        checked: true,
+        optionId: 3,
+        selected: 'A',
+        candiateExamUserId: 4
+      };
+      const updatedStatus: Status = {
+        index: 1,
+        id: 2,
+        checked: false,
+        optionId: 2,
+        selected: 'B',
+        candiateExamUserId: 4
+      };
+      //spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify([existingStatus]));
+      spyOn(localStorage, 'setItem');
+
+      service.storeCheckedStatus(updatedStatus);
+
+      const storedStatuses = JSON.parse(localStorage.getItem('status-array') || '{}');
+      expect(storedStatuses.length).toEqual(1);
+      expect(storedStatuses[0]).toEqual(updatedStatus);
+      expect(localStorage.setItem).toHaveBeenCalledWith('status-array', JSON.stringify([updatedStatus]));
+    });
+  });
+
+  describe('getSelectedAnswer', () => {
+    it('should return the selected answer for a given question index', () => {
+      const status: Status = {
+        index: 1,
+        id: 2,
+        checked: true,
+        optionId: 3,
+        selected: 'A',
+        candiateExamUserId: 4
+      };
+      spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify([status]));
+
+      const answer: SelectedAnswer = service.getSelectedAnswer(1);
+
+      expect(answer).toEqual({ id: 2, optionId: 3, selected: 'A', candiateExamUserId: 4 });
+    });
+
+    it('should return default values if the question index is not found', () => {
+      spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify([]));
+
+      const answer: SelectedAnswer = service.getSelectedAnswer(1);
+
+      expect(answer).toEqual({ id: 0, optionId: 0, selected: '', candiateExamUserId: 0 });
+    });
+  });
+
 });
