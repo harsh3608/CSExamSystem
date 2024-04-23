b74101f7445811fa6e4d9fc11cbf59af8995b840
Harsh Patel <harsh3608@gmail.com>
2024-04-23 10:03:35 +0530

    changes in url link

--- src/app/modules/shared-module/common-server-links.ts
+++ src/app/modules/shared-module/common-server-links.ts
@@ -1,9 +1,20 @@
-export const candidateServerUrl: string = 'http://13.90.224.87:8099/api/Candidate';
+// export const candidateServerUrl: string = 'http://13.90.224.87:8099/api/Candidate';
 
-export const userLoginServerUrl: string = 'http://13.90.224.87:8099/api/Login';
+// export const userLoginServerUrl: string = 'http://13.90.224.87:8099/api/Login';
 
-export const technologyServerUrl: string = 'http://13.90.224.87:8099/api/TechnologyStack';
+// export const technologyServerUrl: string = 'http://13.90.224.87:8099/api/TechnologyStack';
 
-export const questionsServerUrl: string = 'http://13.90.224.87:8099/api/Questions';
+// export const questionsServerUrl: string = 'http://13.90.224.87:8099/api/Questions';
 
-export const candidateExamServerUrl: string = 'http://13.90.224.87:8099/api/CandidateExam';
+// export const candidateExamServerUrl: string = 'http://13.90.224.87:8099/api/CandidateExam';
+
+
+export const candidateServerUrl: string = 'https://localhost:44355/api/Candidate';
+
+export const userLoginServerUrl: string = 'https://localhost:44355/api/Login';
+
+export const technologyServerUrl: string = 'https://localhost:44355/api/TechnologyStack';
+
+export const questionsServerUrl: string = 'https://localhost:44355/api/Questions';
+
+export const candidateExamServerUrl: string = 'https://localhost:44355/api/CandidateExam';
